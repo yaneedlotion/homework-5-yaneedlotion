2b5f6bcec8edcf9c4a2e7417311ecedffe2ab3af
yaneedlotion <97856289+yaneedlotion@users.noreply.github.com>
2022-02-19 14:25:43 -0800

    Added other codes in.

--- listo_lo.py
+++ listo_lo.py
@@ -3,4 +3,17 @@
 
 if __name__ == "__main__":
 
-    food = 
+    food = ['rice', 'beans']
+    print(food)
+    food.append('broccoli')
+    print(food)
+    food.extend(['bread', 'pizza'])
+    print(food)
+    print(food[0:2])
+    print(food[-1])
+
+
+    breakfast = "eggs, fruit, orange juice".split(", ")
+    print(breakfast)
+
+    print(len(breakfast) == 3)
