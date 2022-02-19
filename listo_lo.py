f26dec6f5b9cca0d4b5aa882a56203f8f2328839
yaneedlotion <97856289+yaneedlotion@users.noreply.github.com>
2022-02-19 15:16:54 -0800

    Added code.

--- listo_lo.py
+++ listo_lo.py
@@ -19,4 +19,19 @@ if __name__ == "__main__":
     print(len(breakfast) == 3)
 
 
-    
+    digit_list = []
+    i = 0
+    while i < 1:
+        digit_value = input('floating-point val/stop:').lower()
+        if digit_value == 'stop':
+            print('stopped')
+            print(f'list: {digit_list}')
+            break
+        digit_list.append(float(digit_value))
+        print(f'current list: {digit_list}')
+
+    len(digit_list) > 0
+    avg_list = sum(digit_list) / len(digit_list)
+    print(f'avg of list: {avg_list}')
+    print(f'min val: {min(digit_list)}')
+    print(f'max val: {max(digit_list)}')
